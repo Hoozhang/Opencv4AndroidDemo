cc56cde62ff546a7ffcc1002c285d9a5980bd521
HaoZhang <h.zhang@smail.nju.edu.cn>
2020-12-09 12:49:56 +0800

    init proj

---
+++ app/src/main/java/com/zhao/MainActivity.java
@@ -0,0 +1,68 @@
+package com.zhao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+    private static final String TAG = "MainActivity";
+
+    private CameraBridgeViewBase mCVCamera;
+
+    /**
+     * 通过OpenCV管理Android服务，异步初始化OpenCV
+     */
+    BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status){
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    Log.i(TAG,"OpenCV loaded successfully");
+                    mCVCamera.enableView();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mCVCamera = (CameraBridgeViewBase) findViewById(R.id.camera_view);
+        mCVCamera.setCvCameraViewListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG,"OpenCV library not found!");
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    };
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame frame) {
+        return frame.rgba();
+    }
+
+}
